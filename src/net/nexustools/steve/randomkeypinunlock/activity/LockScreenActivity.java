d92e06bd95fb45b61ef0b3ddd68e40edf200345f
Steve4448 <hohner13@hotmail.com>
2013-10-10 20:16:58 -0400

    Much 'cleaner' layout, tries to fill the bottom of the screen.

--- src/net/nexustools/steve/randomkeypinunlock/activity/LockScreenActivity.java
+++ src/net/nexustools/steve/randomkeypinunlock/activity/LockScreenActivity.java
@@ -10,13 +10,12 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.app.Activity;
 import android.content.Intent;
 
 public class LockScreenActivity extends Activity {
 	public static final int[] ZERO_TO_NINE = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	public RelativeLayout layout;
+	public Button[] pinButtons;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +23,26 @@ public class LockScreenActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_lockscreen);
-		layout = (RelativeLayout)findViewById(R.id.randomPINLayout);
+		pinButtons = new Button[] {
+				(Button)findViewById(R.id.button0),
+				(Button)findViewById(R.id.button1),
+				(Button)findViewById(R.id.button2),
+				(Button)findViewById(R.id.button3),
+				(Button)findViewById(R.id.button4),
+				(Button)findViewById(R.id.button5),
+				(Button)findViewById(R.id.button6),
+				(Button)findViewById(R.id.button7),
+				(Button)findViewById(R.id.button8),
+				(Button)findViewById(R.id.button9)
+		};
 		OnClickListener listener = new OnClickListener() {
 			@Override
             public void onClick(View v) {
 				randomizePINButtons();
             }
 		};
-		for(int i = 0; i < layout.getChildCount() - 1; i++) {
-			((Button)layout.getChildAt(i)).setOnClickListener(listener);
-		}
+		for(Button b : pinButtons)
+			b.setOnClickListener(listener);
 		randomizePINButtons();
 		startService(new Intent(this, LockScreenService.class));
 	}
@@ -42,12 +51,8 @@ public class LockScreenActivity extends Activity {
 		ArrayList<Integer> numbersToAdd = new ArrayList<Integer>(ZERO_TO_NINE.length);
 		for(int i : ZERO_TO_NINE)
 			numbersToAdd.add(i);
-		if(numbersToAdd.size() != layout.getChildCount() - 1)
-			throw new RuntimeException("Malformed layout VS numbers size for the pin numbers?");
 		
-		for(int i = 0; i < layout.getChildCount() - 1; i++) {
-			Button b = (Button)layout.getChildAt(i);
+		for(Button b : pinButtons)
 			b.setText("" + numbersToAdd.remove((int)(Math.random() * numbersToAdd.size())));
-		}
 	}
 }
